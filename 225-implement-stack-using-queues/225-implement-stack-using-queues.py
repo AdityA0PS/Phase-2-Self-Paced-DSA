8c4e7950b9c7e393cc123b7277f28bb2bf2b67cd
AdityA0PS <93821442+AdityA0PS@users.noreply.github.com>
2022-08-10 12:39:45 +0530

    Time: 53 ms (32.59%), Space: 13.9 MB (75.04%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -0,0 +1,19 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        x = self.queue[-1]
+        self.queue = self.queue[:-1:]
+        return x
+
+    def top(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        if not self.queue:
+            return True
